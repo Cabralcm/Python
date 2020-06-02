51f840524f07a8e256431b449e5dd2e24159506e
cabralcm <chris@Chris.localdomain>
2020-06-01 21:43:11 -0400

    p3

--- educative/fsp_1/p3_mod_fuc/p3.py
+++ educative/fsp_1/p3_mod_fuc/p3.py
@@ -98,3 +98,90 @@ def sumList(in_list):
     return [sum+sum for sum in in_list]
 
 print(sumList([1,2,3]))
+
+def findMax(input_list):
+    currentMax = float("-inf")
+    for val in input_list:
+        currentMax = val if (val > currentMax) else currentMax
+    return currentMax
+
+print("Max")
+input = [1,2,3]
+print(findMax(input))
+
+def findMaxIndex(input_list):
+    current = float("-inf")
+    for index, val in enumerate(input_list):
+        if current > val:
+            current = val
+            cur_index = index
+
+
+def findMaxIndex2(input_list):
+    current = float("-inf")
+    cur_index = 0
+    for index in range(len(input_list)):
+        if current < input_list[index]:
+            current = input_list[index]
+            cur_index = index
+    return [current, cur_index]
+
+def findMax3(l):
+    maximum = l[0]
+    for x in l:
+        if x > maximum:
+            maximum = x
+    return maximum
+
+def findMaxIndex3(l):
+    max = l[0]
+    o_index = index
+    for index,x in enumerate(len):
+        if x > max:
+            max = x
+            o_index = index
+    return [max, o_index]
+
+l = [1,2,3]
+[ind, maxi] = findMaxIndex3(l)
+
+def reverse(l):
+    out_list = []
+    for val in range(len(l),0,-1):
+        out_list.append(l[val])
+    print(out_list)
+    return out_list
+
+def reverse2(l):
+    length = len(l)
+    new_list = [None]*length
+    for item in l:
+        length = length -1
+        new_list[length] = item
+    return new_list
+
+def reverse3(l):
+    new_list = l[::-1]
+
+# List splicing
+#l[1:10:2]
+#start 1st index, end 10th index, take steps of 2
+# l[1:10:2] --> 
+#First element is inclusive
+#Second element is exclusive
+#If step size exceeds the slice, 
+# then take the first element referenced
+'''
+Ever since Python 1.4, the slicing syntax 
+has supported an optional third ``step'' or
+ ``stride'' argument. For example, these are
+all legal Python syntax: L[1:10:2], L[:-1:1],
+L[::-1]. This was added to Python at the 
+request of the developers of Numerical Python,
+which uses the third argument extensively. 
+However, Python's built-in list, tuple, and 
+string sequence types have never supported this
+feature, raising a TypeError if you tried it.
+Michael Hudson contributed a patch to fix
+this shortcoming. 
+'''
