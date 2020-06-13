790641cbff862ab222518013b545b4f9eff2dbc7
cabralcm <chris@Chris.localdomain>
2020-06-13 00:15:21 -0400

    generators

--- educative/fsp_1/p7_gen/README.md
+++ educative/fsp_1/p7_gen/README.md
@@ -2,3 +2,54 @@
 
 Generators and Yield
 
+As seen before, iterators are objects that are regularly used with `for loops`. 
+In order words, iterates are objects that implement the **iteration protocol**. 
+
+A Python generator is a convenient way to implement an iterator.
+
+Instead of a class, a generator is a function which returns a value each time the `yield` keyword is used. 
+
+As an example:
+
+```
+def myrange(a,b):
+    while a < b:
+        yield a
+        a += 1
+
+a = myrange(2,4) # call the genreator function which returns an object 
+print(next(a)) # iterate through items using next
+print(next(a))
+```
+
+Akin to iterators, generators can be used with the `for loop`
+
+```
+def myrange(a,b):
+    while a < b:
+        yield a
+        a += 1
+
+for value in myrange(a,b):
+    print (value)
+```
+
+Under the hood, generators behave similarly to iterators. 
+
+The interesting thing about **generators** is the `yield` keyword. 
+The `yield` keyword works much like the `return` keyword, however unlike `return`, it allows the function to eventually RESUME its execution!
+
+In order words, each time the next value of a generator is needed, Python wakes up the function and resumes its execution from the `yield` line as if the function had never executed!
+
+Generator functions can use other functions inside. For instance, it is very common to use the `range` function to iterate over a sequence of numbers.
+
+```
+def squares(n):
+    for value in range(n):
+        yield value * value
+
+sqr = squares(10):
+print(next(sqr))
+print(next(sqr))
+print(next(sqr))
+```
--- educative/fsp_1/p7_gen/p7.py
+++ educative/fsp_1/p7_gen/p7.py
@@ -1,2 +1,93 @@
 # Python Generators
 
+
+def myrange(a,b):
+    while a < b:
+        yield a
+        a += 1
+
+a = myrange(2,4) # call the generator function which returns an object
+# print(next(a)) # iterate through items using next!
+# print(next(a))
+
+# Example with For loop
+
+def myrange(a, b):
+    while a < b:
+      yield a
+      a += 1
+# for value in myrange(1, 4):
+#   print(value)
+
+# Example with using next()
+
+def myrange2(a, b):
+    while a < b:
+      yield a
+      a += 1
+
+seq = myrange(1,3)
+# print(next(seq))
+# print(next(seq)) 
+# next(seq)  Will throw the StopIteration exception
+
+# Example with for loops with generators
+
+def squares(n):
+    for value in range(n):
+        yield value * value
+
+sqr = squares(10)
+# print(next(sqr))
+# print(next(sqr))
+# print(next(sqr))
+# print(next(sqr))
+# print(next(sqr))
+
+# Generator to yield all the ODD numbers from 1 to n
+
+def odd(n):
+    for i in range(1,n+1):
+        if i % 2 != 0:
+            yield i
+        else:
+            continue
+
+# official solution
+
+def odd2(n):
+    for i in range(n):
+        if i % 2 is not 0:
+            yield value
+
+# Test
+n = 10
+# for val in odd(n):
+#     print(val)
+
+def reverse(n):
+    for val in range(n,-1,-1):
+        yield val
+
+# Test
+# for val in reverse(10):
+    # print(val)
+
+# Yield Fib Sequence from 1st to Nth Number
+
+def fibonacci(n):
+    out = []
+    for i in range(n):
+        if i == 0 or i == 1:
+            out.append(i)
+            yield i
+        else:
+            calc = out[i -1] + out[i - 2]
+            out.append(calc)
+            yield calc
+
+# Test
+for i in fibonacci(10):
+    print(i)
+
+
